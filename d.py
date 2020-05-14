bf1029d8e958d009713f626583f08f1dfe3037c9
cryham <cryham@gmail.com>
2020-05-14 23:39:19 +0200

    parse arguments

--- d.py
+++ d.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 #!/usr/bin/env python3
 import os
+from optparse import OptionParser
 
 
 #  start dir
 #---------------------------------------------
 start_dir = os.path.curdir
-start_dir += '/test-dirs/1'
+#start_dir += '/test-dirs/1'
+start_dir += '/test-dirs/2'
 print('---------------------------------------------')
 #print(dir1)
+
+
+#  args
+#---------------------------------------------
+op = OptionParser()
+op.add_option("-t", "--test", dest="test", help="Test only", action="store_true", default=False)
+op.add_option("-s", "--size", dest="size", type="int", default=4096, help="Size to read, -1 full, 0 none")
+
+(opts, args) = op.parse_args()
+
+print("Args: test %r" % opts.test,end='')
+print(", size %d" % opts.size)
+
 
 #  var
 file_set = set()
@@ -26,6 +41,8 @@ def process_file(fpath, fname):
 for root, subdirs, files in os.walk(start_dir):
     print(root)
 
+    #  each dir separate
+    #  todo: don't clear for across dirs
     file_set.clear
 
     for fname in files:
@@ -35,4 +52,3 @@ for root, subdirs, files in os.walk(start_dir):
     print('-----')
     for file in file_set:
         print(file)
-    

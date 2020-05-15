d553430c79d45492db627e7b5be28d0851949e75
cryham <cryham@gmail.com>
2020-05-15 18:06:47 +0200

    small fixes

--- d.py
+++ d.py
@@ -4,21 +4,17 @@ import os
 from optparse import OptionParser
 
 
-#  start dir
-#---------------------------------------------
-start_dir = os.path.curdir
-#start_dir += '/test-dirs/1'
-start_dir += '/test-dirs/2'
-print('---------------------------------------------')
-#print(dir1)
-
-
 #  args
 #---------------------------------------------
-op = OptionParser()
-op.add_option("-t", "--test", dest="test", help="Test only", action="store_true", default=False)
-op.add_option("-s", "--size", dest="size", type="int", default=4096, help="Size to read, -1 full, 0 none, default 4kB")
-op.add_option("-a", "--across", dest="across", help="Test files across dirs", action="store_true", default=False)
+op = OptionParser(description='Find duplicates and delete, rename') #, usage='',epilog='')
+op.add_option("-d", "--dir", dest="dir",
+                    help="Path, if not set, uses current", default='')
+op.add_option("-t", "--test", dest="test",
+                    help="Test only", action="store_true", default=False)
+op.add_option("-s", "--size", dest="size", type="int", default=4096,
+                    help="Size to read, -1 full, 0 none, default 4kB")
+op.add_option("-a", "--across", dest="across",
+                    help="Test files across dirs", action="store_true", default=False)
 
 (opts, args) = op.parse_args()
 
@@ -26,13 +22,24 @@ print("Args: test %r" % opts.test,end='')
 print(", across %r" % opts.across,end='')
 print(", size %d" % opts.size)
 
+#  start dir
+if opts.dir == '':
+    start_dir = os.getcwd()
+    #start_dir += '/test-dirs/1'
+    start_dir += '/test-dirs/2'
+else:
+    start_dir = opts.dir
+
+print("Path: " + start_dir)
+print('---------------------------------------------')
 
 #  var
 file_list = list()  # all files
 file_count = dict()  # same file properties count for unique
 
 #  stats
-class stats:
+class cstats:
+    all_dirs = 0
     all_files = 0
     left_files = 0  # left after deleting duplicates
     all_size = 0
@@ -45,12 +52,13 @@ class cfile:
     unique = True
     same_count = 0
 
+stats = cstats()
+
 
 #  process 1 file
 #---------------------------------------------
 def process_file(fpath, fname):
     #print(fpath)
-    global stats
 
     fspl = os.path.splitext(fname)
     fne = fspl[0]  # fname no ext
@@ -59,22 +67,21 @@ def process_file(fpath, fname):
     size = os.path.getsize(fpath)
     stats.all_files += 1
     stats.all_size += size
-    
     #print(fname + '  ' + str(size) + '  ' + fne + ' ' + ext)
 
     #  find (n) in name
     l = fne.rfind('(')
     r = fne.rfind(')')
 
-    if l+1 < r and l != -1 and r != -1:
+    if l+1 < r and l > 0 and r > 0:
         n = fne[l+1:r]  # num (n)
         fnb = fne[:l]  # fname no ()
         fne = fnb
-    #print('no ' + fne +' '+ str(size) +' '+ str(l)+' '+str(r))
+    #print('l '+str(l)+' r '+str(r))
     
-    
+
     #  file properties needed to be different for being unique
-    unique_file = (fne, size)  #, hash)
+    unique_file = (fne, ext, size)  #, hash)
     in_count = file_count.get(unique_file,0)
     file_count[unique_file] = in_count + 1  # inc count
 
@@ -94,7 +101,7 @@ def process_file(fpath, fname):
     cf.unique = unique
     
     #  info
-    print(cf.path+'  '+cf.fne+'  '+cf.ext+'  s: '+str(cf.size)+'  u: '+str(unique))
+    print(fname+'  '+cf.fne+cf.ext+'  s: '+str(cf.size)+'  u: '+str(unique))
     #print(fne+'  s '+str(size)+' '+str(unique))
 
     #  add
@@ -106,6 +113,7 @@ def process_file(fpath, fname):
 #---------------------------------------------
 for root, subdirs, files in os.walk(start_dir):
     print(root+'/  ----')
+    stats.all_dirs += 1
 
     #  each dir separate
     #  don't clear for across dirs
@@ -117,17 +125,17 @@ for root, subdirs, files in os.walk(start_dir):
         fpath = os.path.join(root, fname)
         process_file(fpath, fname)
 
-    print('-----')
+    #print('-----')
 
-    #  action loop
-    #if not opts.test:
-    #for file in file_set:
+print('----- stats -----')
+print(' dirs:  ' + str(stats.all_dirs))
+print('files:  {:.2f}'.format(100.0 * stats.left_files / stats.all_files) + '%  ' + str(stats.left_files) + ' / ' + str(stats.all_files))
+print(' size:  {:.2f}'.format(100.0 * stats.left_size / stats.all_size) + '%  ' + str(stats.left_size) + ' / ' + str(stats.all_size) + ' B')
+
+#  delete files
+if not opts.test:
     for file in file_list:
         #print(file.path)
         #if not file.unique:
         #  delete
         pass
-
-print('----- stats -----')
-print('files:  {:.2f}'.format(100.0 * stats.left_files / stats.all_files) + '%  ' + str(stats.left_files) + ' / ' + str(stats.all_files))
-print(' size:  {:.2f}'.format(100.0 * stats.left_size / stats.all_size) + '%  ' + str(stats.left_size) + ' / ' + str(stats.all_size) + ' B')
